8b39f2e681f07acefdbb5c910cc18fe63b89747a
Dan Brevetti <dbrevetti@mitre.org>
2017-08-08 14:59:13 -0400

    changed world to earth

--- helloworld.java
+++ helloworld.java
@@ -1,15 +1,15 @@
-public class HelloWorld { 
+public class HelloEarth { 
     public static void main(String[] args) { 
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World"); 
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth"); 
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earth"); 
     }
 }
 
