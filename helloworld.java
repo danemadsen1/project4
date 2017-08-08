e8315c070de4b4d251fb83cdc42a70a05e0731b0
Dane Madsen <dmadsen@mitre.org>
2017-08-08 15:14:20 -0400

    changed to Earthling

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
-public class HelloEarth { 
+public class HelloEarthling { 
     public static void main(String[] args) { 
-        System.out.println("Hello, Earth");
+        System.out.println("Hello, Earthling");
         System.out.println("Hello, Earth");
         System.out.println("Hello, Earth");
         System.out.println("Hello, Earth");
